2743af79a1b2162570081e1fd93ec3741d4a6a75
Joel Plane <joel@avial.com.au>
2011-06-12 18:02:41 +1000

    Extracted Context class to own file

---
+++ context.js
@@ -0,0 +1,43 @@
+module.exports = (function(Test){
+  var EventEmitter = require('events').EventEmitter;
+  var sys = require('sys');
+  function Context(name, parentContext) {
+    this.name = name;
+    this.parentContext = parentContext;
+    this.subContexts = [];
+    this.tests = [];
+    EventEmitter.call(this);
+    Context.emit('new', this);
+  }
+  sys.inherits(Context, EventEmitter);
+  /* class-level event emitter for Context class */
+  (function(){
+    var emitter = new EventEmitter();
+    Context.on = function(eventName, callback) {
+      emitter.on(eventName, callback);
+    };
+    Context.emit = function(eventName, object) {
+      emitter.emit(eventName, object);
+    };
+  })();
+  Context.prototype.context = function(name, callback) {
+    var ctx = new Context(name, this);
+    this.subContexts.push(ctx);
+    this.emit('pushContext', {name: name, context: ctx});
+    callback.apply(ctx);
+    this.emit('popContext', {name: name, context: ctx});
+  };
+  Context.prototype.it = function(name, callback) {
+    this.tests.push(new Test(this, name, callback));
+  };
+  Context.prototype._depth = function() {
+    var parent = this;
+    var i = 0;
+    while(parent) {
+      parent = parent.parentContext;
+      i++;
+    }
+    return i;
+  };
+  return Context;
+});
--- noodleTest.js
+++ noodleTest.js
@@ -102,44 +102,7 @@ module.exports = (function(config){
         main.emit('testFlunk', {context: this.context, message: message, options: options});
     };
 
-    var Context = function(name, parentContext) {
-      this.name = name;
-      this.parentContext = parentContext;
-      this.subContexts = [];
-      this.tests = [];
-      EventEmitter.call(this);
-      Context.emit('new', this);
-    };
-    sys.inherits(Context, EventEmitter);
-    /* class-level event emitter for Context class */
-    (function(){
-      var emitter = new EventEmitter();
-      Context.on = function(eventName, callback) {
-        emitter.on(eventName, callback);
-      };
-      Context.emit = function(eventName, object) {
-        emitter.emit(eventName, object);
-      };
-    })();
-    Context.prototype.context = function(name, callback) {
-      var ctx = new Context(name, this);
-      this.subContexts.push(ctx);
-      this.emit('pushContext', {name: name, context: ctx});
-      callback.apply(ctx);
-      this.emit('popContext', {name: name, context: ctx});
-    };
-    Context.prototype.it = function(name, callback) {
-      this.tests.push(new Test(this, name, callback));
-    };
-    Context.prototype._depth = function() {
-      var parent = this;
-      var i = 0;
-      while(parent) {
-        parent = parent.parentContext;
-        i++;
-      }
-      return i;
-    };
+    var Context = require('./context')(Test);
 
     /* Relay events emitted by Context instances to the main object */
     Context.on('new', function(ctx){
